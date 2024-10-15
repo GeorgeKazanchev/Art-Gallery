9e76605fea386ddfdf0e130b8cdfdcf81a1ed63d
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-10-15 21:42:38 +0300

    Добавлена заготовка поля поиска; добавлены отступы внизу страницы

--- src/features/search/ui/search.tsx
+++ src/features/search/ui/search.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import * as styles from './search.module.scss';
 
 export const Search: React.FC = () => {
   return (
-    <React.Fragment></React.Fragment>
+    <form className={styles.searchForm} method='get' action='#'>
+      <input className={styles.input} type='text' name='search' id='' placeholder='Painting title' />
+    </form>
   );
 };
