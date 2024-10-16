8e3327d842eee746d418c8d9f7a1256d3e81fcc7
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-10-16 20:06:41 +0300

    Добавлена светлая тема для галереи

--- src/entities/painting-card/ui/painting-card.tsx
+++ src/entities/painting-card/ui/painting-card.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import * as styles from './painting-card.module.scss';
+import { useAppSelector } from '../../../shared/model/redux-hooks';
+import { selectTheme } from '../../../shared/model/theme-slice';
+import { isThemeLight } from '../../../shared/helpers/theme';
 import type { Painting } from '../../../shared/types/painting';
 
 type Props = {
@@ -7,26 +10,29 @@ type Props = {
 };
 
 export const PaintingCard: React.FC<Props> = ({ painting }) => {
+  const curTheme = useAppSelector(selectTheme);
+  const isLight = isThemeLight(curTheme);
+
   return (
     <figure className={styles.card}>
       <img className={styles.image} src={painting.imgUrl} alt={painting.name} />
-      <figcaption className={styles.caption}>
+      <figcaption className={`${styles.caption} ${isLight ? styles.captionLight : ''}`}>
         <div className={styles.descriptionWrapper}>
 
           <div className={`${styles.description} ${styles.descriptionMain}`}>
-            <h2 className={`${styles.upperLine} ${styles.name}`}>
+            <h2 className={`${styles.upperLine} ${styles.name} ${isLight ? styles.upperLineLight : ''}`}>
               {painting.name}
             </h2>
-            <span className={styles.lowerLine}>
+            <span className={`${styles.lowerLine} ${isLight ? styles.lowerLineLight : ''}`}>
               {painting.created}
             </span>
           </div>
 
           <div className={`${styles.description} ${styles.descriptionAdditional}`}>
-            <span className={styles.upperLine}>
+            <span className={`${styles.upperLine} ${isLight ? styles.upperLineLight : ''}`}>
               {painting.author.name}
             </span>
-            <span className={styles.lowerLine}>
+            <span className={`${styles.lowerLine} ${isLight ? styles.lowerLineLight : ''}`}>
               {painting.location.name}
             </span>
           </div>
