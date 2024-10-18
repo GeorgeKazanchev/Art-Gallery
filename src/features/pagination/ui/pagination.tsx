4cee98a4dac429a3d30e7768112878f551b21176
George Kazanchev <127313091+GeorgeKazanchev@users.noreply.github.com>
2024-10-18 20:11:28 +0300

    Доработана пагинация
    1. Ссылки перехода на соседние страницы заменены на кнопки. При нахождении на первой странице блокируется кнопка "Назад". При нахождении на последней - кнопка "Вперёд".
    
    2. В качестве значения по умолчанию установлена первая страница.

--- src/features/pagination/ui/pagination.tsx
+++ src/features/pagination/ui/pagination.tsx
@@ -10,35 +10,36 @@ import getPageClassname from '../helpers/get-page-classname';
 
 type Props = {
   pagesCount: number,
-  checkedPage: number,
+  currentPage: number,
 };
 
 const getPaginationPages = createPaginator(MAX_PAGES_SHOWN, ELLIPSIS);
 
-export const Pagination: React.FC<Props> = ({ pagesCount, checkedPage }) => {
-  if (checkedPage < 0 || checkedPage > pagesCount) {
+export const Pagination: React.FC<Props> = ({ pagesCount, currentPage }) => {
+  if (currentPage < 0 || currentPage > pagesCount) {
     throw new RangeError('Active page in the pagination is incorrect');
   }
 
-  const paginationPages = getPaginationPages(pagesCount, checkedPage);
+  const paginationPages = getPaginationPages(pagesCount, currentPage);
   const curTheme = useAppSelector(selectTheme);
   const isLight = isThemeLight(curTheme);
 
   return (
     <div className={styles.pagination}>
-      <a
+      <button
         className={`${styles.adjacentPage} ${styles.previousPage} ${isLight ? styles.adjacentPageLight : ''}`}
-        href='#'
+        type='button'
+        disabled={currentPage === 1}
         aria-label='Go to the previous page'
       >
         <svg className={`${styles.pageLinkIcon} ${isLight ? styles.pageLinkIconLight : ''}`} width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M0.312255 0.283494C0.487119 0.0697714 0.80213 0.0382703 1.01585 0.213134L7.61585 5.61313C7.73192 5.7081 7.79923 5.85015 7.79923 6.00011C7.79923 6.15008 7.73192 6.29213 7.61585 6.38709L1.01585 11.7871C0.80213 11.962 0.487119 11.9305 0.312255 11.7167C0.137391 11.503 0.168893 11.188 0.382615 11.0131L6.50964 6.00011L0.382615 0.987091C0.168892 0.812228 0.137391 0.497216 0.312255 0.283494Z" />
         </svg>
-      </a>
+      </button>
 
       <ul className={`${styles.pages} ${isLight ? styles.pagesLight : ''}`}>
         {paginationPages.map((page, index) => (       //  TODO: Change keys to something else
-          <li key={index} className={getPageClassname(page, checkedPage, isLight)}>
+          <li key={index} className={getPageClassname(page, currentPage, isLight)}>
             {page !== ELLIPSIS
               ? (
                 <a className={`${styles.pageLink}`} href='#'>{page}</a>
@@ -48,15 +49,16 @@ export const Pagination: React.FC<Props> = ({ pagesCount, checkedPage }) => {
         ))}
       </ul>
 
-      <a
+      <button
         className={`${styles.adjacentPage} ${isLight ? styles.adjacentPageLight : ''}`}
-        href='#'
+        type='button'
+        disabled={currentPage === pagesCount}
         aria-label='Go to the next page'
       >
         <svg className={`${styles.pageLinkIcon} ${isLight ? styles.pageLinkIconLight : ''}`} width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M0.312255 0.283494C0.487119 0.0697714 0.80213 0.0382703 1.01585 0.213134L7.61585 5.61313C7.73192 5.7081 7.79923 5.85015 7.79923 6.00011C7.79923 6.15008 7.73192 6.29213 7.61585 6.38709L1.01585 11.7871C0.80213 11.962 0.487119 11.9305 0.312255 11.7167C0.137391 11.503 0.168893 11.188 0.382615 11.0131L6.50964 6.00011L0.382615 0.987091C0.168892 0.812228 0.137391 0.497216 0.312255 0.283494Z" />
         </svg>
-      </a>
+      </button>
     </div>
   );
 };
